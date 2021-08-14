e2c665c5707f3c6a05c60d2565b01e46c8389ab8
bo-domi <86868222+bo-domi@users.noreply.github.com>
2021-08-14 19:01:34 +0900

    バリデーション等設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :email, presence: true
+         validates :encrypted_password, presence: true
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         validates :last_name_canat, presence: true
+         validates :first_name_canat, presence: true
+         validates :birthday, presence: true
+
+         has_many :items
+         has_many :records
+
 end
