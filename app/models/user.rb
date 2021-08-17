c7506def2ebc352ee1cde74717cd668c5fc94377
bo-domi <86868222+bo-domi@users.noreply.github.com>
2021-08-17 21:04:32 +0900

    単体テストコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :email, presence: true
-         validates :encrypted_password, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_canat, presence: true
-         validates :first_name_canat, presence: true
-         validates :birthday, presence: true
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'を入力してください' } 
+        
+         with_options presence: true do
+          validates :nickname
+         validates :birthday
+        end
 
+        with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）で入力してください' } do
+          validates :last_name
+          validates :first_name
+        end
+        
+        with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（カタカナ）で入力してください' } do
+          validates :last_name_canat
+          validates :first_name_canat
+        end
+
+    
          has_many :items
          has_many :records
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'items#index'
-  resources :items, only: [:index, :show, :new, :create, :destroy, :edit, :update,] do
-    
-  end
+  #resources :items, only: [:index, :show, :new, :create, :destroy, :edit, :update,] do
+
+  #end
 
 
 end
