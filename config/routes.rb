fac6559ce65c0b07b56bd24c435b6250fecf398c
bo-domi <86868222+bo-domi@users.noreply.github.com>
2021-08-17 12:54:25 +0900

    ログイン、ログアウト機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'items#index'
+  resources :items, only: [:index, :show, :new, :create, :destroy, :edit, :update,] do
+    
+  end
 
 
 end
