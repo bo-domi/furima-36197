d37b5229e88d669642b60bf029f031d5f3c791e5
bo-domi <86868222+bo-domi@users.noreply.github.com>
2021-08-21 19:12:15 +0900

    ActiveHashモデル追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
   has_one :record
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :detail
+  belongs_to :delivery_price
+  belongs_to :area
+  belongs_to :day
 end
