3a7adb42426faa26d3f94011fc485b005e7bb0c6
bo-domi <86868222+bo-domi@users.noreply.github.com>
2021-08-21 14:30:46 +0900

    リダイレクト機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
 
   def index
    @items = Item.all
@@ -9,13 +10,34 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+  @item = Item.new(item_params)
+  if @item.save
+    redirect_to action: "show"
+  else
+    render :index
   end
+end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
 
 private
 
 def itme_params
   params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
 
+
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'items#index'
-  resources :items, only: [:index, :new, :create ] do
+  resources :items, only: [:index, :new, :create, :show, :destroy, :edit ] do
   end
-
-  root to: 'articles#index'
-  resources :articles
-
 end
