49711e5dbee36893da1b3f3b4665e179f44c6e55
bo-domi <86868222+bo-domi@users.noreply.github.com>
2021-09-10 21:22:09 +0900

    建物名修正

--- spec/factories/record_addresses.rb
+++ spec/factories/record_addresses.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     municipality                     {'横浜市緑区'}
     house_number                     {'青山1-1-1'}
     telephone_number                 {'09012345678'}
-    building_name                    {''}
+    building_name                    {'柳ビル101'}
     token                            {'tok_abcdefghijk00000000000000000'}
   end
 end
